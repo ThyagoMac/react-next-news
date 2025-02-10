61e3d39d31ecae91c4262fc7dc08a19042fdde91
Thyago Mac Cormick <32471417+ThyagoMac@users.noreply.github.com>
2025-02-10 12:31:21 +0000

    Feat: adiciona arquivo .env e configurações do PostgreSQL no docker-compose; adiciona dependência pg e atualiza scripts de teste

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(request, response) {
+//pages/api/v1/status/index.js
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT NOW()");
+  console.log("result: ", result);
   response.status(200).json({ status: "OK" });
 }
 export default status;
