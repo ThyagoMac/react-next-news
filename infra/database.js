2036fc8b6140dca0ea7cbf3b8f512f95ea12032e
Thyago Mac Cormick <32471417+ThyagoMac@users.noreply.github.com>
2025-03-27 11:44:20 +0000

    refatora conexão com o banco de dados e melhora o gerenciamento de migrações

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,11 @@
 //infra/database.js
 import { Client } from "pg";
 
-const query = async (params) => {
-  const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    ssl: false,
-    //ssl: process.env.NODE_ENV === "production",
-  });
-
+const query = async (queryObject) => {
+  let client;
   try {
-    await client.connect();
-    const result = await client.query(params);
+    client = await getNewClient();
+    const result = await client.query(queryObject);
     return result;
   } catch (err) {
     console.error("Erro na conexão/consulta do banco de dados:");
@@ -22,12 +13,37 @@ const query = async (params) => {
     console.error(`Database: ${process.env.POSTGRES_DB}`);
     console.error(`User: ${process.env.POSTGRES_USER}`);
     console.error("Detalhes do erro:", err.message);
-    throw new Error("Erro ao acessar o banco de dados: " + err.message);
+    throw err;
   } finally {
     await client.end();
   }
 };
 
+const getNewClient = async () => {
+  const client = new Client({
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: getSSLValues(),
+    //ssl: process.env.NODE_ENV === "production",
+  });
+  await client.connect();
+  return client;
+};
+
 export default {
-  query: query,
+  query,
+  getNewClient,
+};
+
+const getSSLValues = () => {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
 };
--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,18 +1,49 @@
+import database from "@/infra/database";
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 
 export default async function migrations(request, response) {
-  try {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: true,
-      dir: join(process.cwd(), "infra", "migrations"),
-      direction: "up",
-      migrationsTable: "pgmigrations",
-      verbose: true,
-    });
-    response.status(200).json(migrations);
-  } catch (error) {
-    response.status(500).json({ error: error.message });
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationOptions = {
+    dbClient: dbClient,
+    databaseUrl: process.env.DATABASE_URL,
+    dryRun: true,
+    dir: join(process.cwd(), "infra", "migrations"),
+    direction: "up",
+    migrationsTable: "pgmigrations",
+    verbose: true,
+  };
+  if (request.method === "POST") {
+    try {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationOptions,
+        dryRun: false,
+      });
+
+      if (migratedMigrations.length > 0) {
+        response.status(201).json(migratedMigrations);
+      }
+      response.status(200).json(migratedMigrations);
+    } catch (error) {
+      response.status(500).json({ error: error.message });
+    } finally {
+      await dbClient.end();
+      return;
+    }
   }
+
+  if (request.method === "GET") {
+    try {
+      const pendingMigrations = await migrationRunner(defaultMigrationOptions);
+      response.status(200).json(pendingMigrations);
+    } catch (error) {
+      response.status(500).json({ error: error.message });
+    } finally {
+      await dbClient.end();
+      return;
+    }
+  }
+
+  return response.status(405).end();
 }
